74d5823339ba1443302b88f78e48523ce0c76554
Andres <134742998+andreshamburger@users.noreply.github.com>
2024-05-24 13:45:20 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Header
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
+
+// scroll to top button
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("gotopbtn");
+  let progressValue = document.getElementById("gotopbtn-filled");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#3548f4,#01c4fa ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
